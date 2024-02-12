c344bc985fba45ae81fc6b33a7ee6a56a29437d1
bobobo34 <evinlodder@gmail.com>
2024-02-11 19:57:02 -0500

    init

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+class Student
+{
+    private String name;
+    private int age;
+    private double gpa;
+    public Student (String n, int a, double g)
+    {
+        name = n;
+        age = a;
+        gpa = g;
+    }
+    public String getName() { return name; }
+    public int getAge() { return age; }
+    public double getGPA() { return gpa; }
+    public String toString()
+    {
+        String temp = name + " " + age + " " + gpa + "\n";
+        return temp;
     }
 }
